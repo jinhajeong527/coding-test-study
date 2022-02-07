b157496b3f169f38581d923262c9606f49108144
jinha <jinha4ever@gmail.com>
2022-02-07 21:42:16 +0900

    프로그래머스 1단계: 2016년

--- CodingTestStudy/src/com/jjh/study/stage/one/Year2016.java
+++ CodingTestStudy/src/com/jjh/study/stage/one/Year2016.java
@@ -2,23 +2,20 @@ package com.jjh.study.stage.one;
 
 public class Year2016 {
 	public String solution(int a, int b) {
+
 		int[] months = new int[]{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		String[] days = new String[] {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-		//1월 1일
-		//월에서는 아무것도 가져오면 안되고, +b 하면 1보다 작다.. 총 값은 1이 된다.
 		int sum =0;
-		
+		//1월 일때는 b 값만 sum에 더해져야 하므로, 길이는 a-1이 된다.
 		for(int i=0; i<a-1; i++) {
 			sum += months[i];
 		}
-		System.out.println(sum);//121
-		sum += b;//145
-		//2016년 2월 1일 월요일 sum은 32..
-		int index = 32%7;//4...
-		System.out.println(index);
-		String answer = days[index];
-		System.out.println(answer);
-		return answer;
+		sum += b;
+
+		int index = sum%7;//0,1,2,3,4,5,6 이렇게 나올 것.
+		if(index ==0) index =7; //index=0일 때는 -1 해주면 존재하지 않는 인덱스에 접근하게 되므로 7로 바꿔준다.
+
+		return days[index-1];
     }
 
 }
