7cbdac9c2121ad8173a69e0ffe8ae6bf6f3f02fe
jinha <jinha4ever@gmail.com>
2022-02-12 14:03:28 +0900

    프로그래머스 1단계: 나머지가 1이 되는 수 찾기

---
+++ CodingTestStudy/src/com/jjh/study/stage/one/FindingSmallestNumberWithRemainderOfOne.java
@@ -0,0 +1,18 @@
+package com.jjh.study.stage.one;
+
+public class FindingSmallestNumberWithRemainderOfOne {
+
+    public int solution(int n) {
+        int answer = 0;
+        int x = 2;
+        while(true){
+            if(n%x==1) {
+                answer = x;
+                break;
+            }
+            x ++;
+        }
+        return answer;
+
+    }
+}
