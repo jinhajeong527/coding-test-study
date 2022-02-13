c220f050ec70f4e24eb28fcb4f952fe5c9cbc8b0
jinha <jinha4ever@gmail.com>
2022-02-13 21:21:53 +0900

    프로그래머스 1단계: K번째 수

---
+++ CodingTestStudy/src/com/jjh/study/stage/one/KthNumber.java
@@ -0,0 +1,20 @@
+package com.jjh.study.stage.one;
+
+import java.util.Arrays;
+
+//K번째 수
+public class KthNumber {
+    public static void main(String[] args) {
+        KthNumber sw = new KthNumber();
+        sw.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}});
+    }
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length; i++) {
+           int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+           Arrays.sort(arr);
+           answer[i] =   arr[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
