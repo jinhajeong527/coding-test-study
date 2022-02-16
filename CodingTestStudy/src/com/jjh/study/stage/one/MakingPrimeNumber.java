e438dd3120cbb4769e6bf6e6bf17f6206cda6a0b
jinha <jinha4ever@gmail.com>
2022-02-16 20:42:12 +0900

    프로그래머스 1단계: 소수만들기

---
+++ CodingTestStudy/src/com/jjh/study/stage/one/MakingPrimeNumber.java
@@ -0,0 +1,30 @@
+package com.jjh.study.stage.one;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//소수만들기: 중복값도 가능하다.
+public class MakingPrimeNumber {
+    public int solution(int[] nums) {
+        int answer = 0;
+        for(int i=0; i < nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    int sum = nums[i]+nums[j]+nums[k];
+                    if(isPrimeNumber(sum)){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+        public boolean isPrimeNumber(int num){
+            for(int m = 2; m < num; m++) {
+                if (num % m == 0) return false;
+            }
+            return true;
+        }
+
+}
