d22a1cadffee2804ec080068dd8d69f50c8792cc
jinha <jinha4ever@gmail.com>
2022-03-05 08:59:03 +0900

    프로그래머스 2단계: 가장큰수

--- CodingTestStudy/src/com/jjh/study/stage/two/TheBiggestNumberPossible.java
+++ CodingTestStudy/src/com/jjh/study/stage/two/TheBiggestNumberPossible.java
@@ -1,47 +1,38 @@
 package com.jjh.study.stage.two;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TheBiggestNumberPossible {
 	public static void main(String[] args) {
 		TheBiggestNumberPossible tp = new TheBiggestNumberPossible();
-		tp.solution(new int[]{ 30, 314, 34, 5, 9});
+		tp.solution(new int[]{ 1, 10, 100, 1000});
 	}
 	public String solution(int[] numbers) {
 		
 		String[] intArr = new String[numbers.length];
+
 		for(int i = 0; i < numbers.length; i++) {
 			intArr[i] = Integer.toString(numbers[i]);
 		}
 		
-		Arrays.sort(intArr);
-		
-//		for(int i=0; i<intArr.length; i++) {
-//			System.out.println(intArr[i]);
-//		}
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = intArr.length-1; i >= 0; i--) {
-			if( i-1>=0 && intArr[i].length() > intArr[i-1].length()) {
-				String str1 = intArr[i].substring(intArr[i].length()-1, intArr[i].length());
-				String str2 = intArr[i-1].substring(0,1);
-				if(Integer.valueOf(str1)<Integer.valueOf(str2)) {
-					
-					sb.append(intArr[i-1]);
-					sb.append(intArr[i]);
-					i--;
-				}else {
-					sb.append(intArr[i]);
-					sb.append(intArr[i-1]);
-					i--;
-				}
-			}else {
-				sb.append(intArr[i]);
+		//내림차순 정렬이다.
+		Arrays.sort(intArr, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return (o2+o1).compareTo(o1+o2);
 			}
+		});
+
+		if(intArr[0].equals("0")) return "0";
+
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 0; i < intArr.length; i++) {
+			sb.append(intArr[i]);
 		}
-		String answer = sb.toString();
-		//System.out.println(answer);
-		return answer;
+
+		return sb.toString();
 	}
 
 }
