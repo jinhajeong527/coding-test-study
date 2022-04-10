417cc00a6fe921beb4aa9897361aee41d179356a
jinha <jinha4ever@gmail.com>
2022-04-10 11:36:11 +0900

    리트코드 : Counting Bits

---
+++ CodingTestStudy/src/com/jjh/study/leet/easy/dynamic/CountingBits2.java
@@ -0,0 +1,15 @@
+package com.jjh.study.leet.easy.dynamic;
+
+//DP 방식으로 풀어보기
+public class CountingBits2 {
+    public int[] countBits(int n) {
+
+        int[] bits = new int[n+1];
+        bits[0] = 0;
+
+        for(int i = 1;i <= n; i++) {
+            bits[i] = bits[i/2] + i%2;
+        }
+        return bits;
+    }
+}
