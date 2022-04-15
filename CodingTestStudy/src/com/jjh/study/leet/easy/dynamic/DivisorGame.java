236d7ac5e17e6dcb1e2c160a60a37a6cf3a7ec14
jinha <jinha4ever@gmail.com>
2022-04-15 11:14:59 +0900

    리트코드 : Divisor Game

---
+++ CodingTestStudy/src/com/jjh/study/leet/easy/dynamic/DivisorGame.java
@@ -0,0 +1,25 @@
+package com.jjh.study.leet.easy.dynamic;
+
+public class DivisorGame {
+	
+	public boolean divisorGame(int n) {
+		
+		boolean[] dp = new boolean[n+1];
+		dp[0] = false;
+		dp[1] = false;
+		
+		for(int i = 2; i <= n; i++) {
+			for(int j = 1; j < i; j++) {
+				if(i % j == 0) {
+					if(dp[i-j] == false) {
+						dp[i] = true;
+						break;
+					}
+				}
+			}
+		}
+		
+        return dp[n];
+    }
+	
+}
