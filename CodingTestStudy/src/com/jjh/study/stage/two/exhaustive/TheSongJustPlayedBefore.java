46cec7acb1cf4e214628a055398af6a2e13c2c34
jinha <jinha4ever@gmail.com>
2022-06-26 09:19:18 +0900

    프로그래머스 2단계: 방금그곡

--- CodingTestStudy/src/com/jjh/study/stage/two/exhaustive/TheSongJustPlayedBefore.java
+++ CodingTestStudy/src/com/jjh/study/stage/two/exhaustive/TheSongJustPlayedBefore.java
@@ -1,4 +1,4 @@
-package com.jjh.study.stage.two;
+package com.jjh.study.stage.two.exhaustive;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,15 +8,16 @@ import java.util.Date;
 public class TheSongJustPlayedBefore {
 	public static void main(String[] args) {
 		TheSongJustPlayedBefore sp = new TheSongJustPlayedBefore();
-		sp.solution("CC#BCC#BCC#", new String[] { "03:00,03:08,FOO,CC#B", "23:00,00:00,BELLO,A#" });
+		sp.solution("CC#BCC#BCC#", new String[] { "03:00,03:08,FOO,CC#B", "23:00,23:59,BELLO,A#" });
 	}
 
 	public String solution(String m, String[] musicinfos) {
 		int maxPlayTime = -1;
+		// C# 과 같이 한 음이면서 한글자가 아닌 음을 한글자의 대문자 알파벳으로 바꿔주는 메서드
 		m = changeMelodyWithNumberSign(m);
 		String answer = "";
 		for (int i = 0; i < musicinfos.length; i++) {
-			
+			// 배열 String 예시 데이터 : "12:00,12:14,HELLO,CDEFGAB"
 			String[] infoSplits = musicinfos[i].split(",");
 			SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
 			long minDiff = 0;
@@ -24,13 +25,11 @@ public class TheSongJustPlayedBefore {
 			try {
 				Date startTime = dateFormat.parse(infoSplits[0]);
 				Date endTime = dateFormat.parse(infoSplits[1]);
-			
-				minDiff = (endTime.getTime() - startTime.getTime())/60000;
+				minDiff = (endTime.getTime() - startTime.getTime()) / 60000;
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
-		
-			
+
 			infoSplits[3] = changeMelodyWithNumberSign(infoSplits[3]);
 			char[] melodyArr = infoSplits[3].toCharArray();
 			StringBuilder sb = new StringBuilder();
@@ -38,32 +37,26 @@ public class TheSongJustPlayedBefore {
 			for(int j = 0; j < minDiff; j++) {
 				sb.append(melodyArr[index]);
 				index++;
-				if(index==melodyArr.length) index = 0;
+				if(index == melodyArr.length) index = 0;
 			}
-			
+			// 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환
 			if(sb.toString().contains(m) && minDiff > maxPlayTime) {
 				 answer = infoSplits[2];
 				 maxPlayTime = (int) minDiff;
 			}
 
 		}
-		System.out.println(answer);
 		return maxPlayTime != -1 ? answer : "(None)";
 	}
 	
 	public String changeMelodyWithNumberSign(String melody) {
-		if(melody.contains("C#")) {
-			melody = melody.replaceAll("C#", "H");
-		}else if(melody.contains("D#")) {
-			melody = melody.replaceAll("D#", "I");
-		}else if(melody.contains("F#")) {
-			melody = melody.replaceAll("F#", "J");		
-		}else if(melody.contains("G#")) {
-			melody = melody.replaceAll("G#", "K");		
-		}else if(melody.contains("A#")) {
-			melody = melody.replaceAll("A#", "L");		
-		}
+
+		melody = melody.replaceAll("C#", "H");
+		melody = melody.replaceAll("D#", "I");
+		melody = melody.replaceAll("F#", "J");
+		melody = melody.replaceAll("G#", "K");
+		melody = melody.replaceAll("A#", "L");
+
 		return melody;
-		
 	}
 }
