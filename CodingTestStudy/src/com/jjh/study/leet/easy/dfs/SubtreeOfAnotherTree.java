7ea98700ed3549de631a3cfb1694ce5fab1ea405
jinha <jinha4ever@gmail.com>
2022-07-07 12:52:05 +0900

    리트코드 easy : 572. Subtree of Another Tree

--- CodingTestStudy/src/com/jjh/study/leet/easy/dfs/SubtreeOfAnotherTree.java
+++ CodingTestStudy/src/com/jjh/study/leet/easy/dfs/SubtreeOfAnotherTree.java
@@ -5,30 +5,24 @@ import com.jjh.study.ds.TreeNode;
 public class SubtreeOfAnotherTree {
     public static void main(String[] args) {
         SubtreeOfAnotherTree soat = new SubtreeOfAnotherTree();
-        TreeNode root = new TreeNode(1, null, null);
-        TreeNode subRoot = new TreeNode(0, null, null);
+        TreeNode root = new TreeNode(3, null, null);
+        root.left = new TreeNode(4, new TreeNode(1, null, null), null);
+        root.right = new TreeNode(5, new TreeNode(2, null, null), null);
+        TreeNode subRoot = new TreeNode(3, new TreeNode(1, null, null), new TreeNode(2, null, null));
         System.out.println(soat.isSubtree(root, subRoot));
 
     }
-    boolean isOnlyOne = false;
+    
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(subRoot.left == null && subRoot.right == null) {
-            isOnlyOne = true;
-            if(root.left == null && root.right == null && root.val != subRoot.val) return false;
-        }
-
-        return dfs(root, subRoot);
+        if(root == null) return false;
+        if(dfs(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot); 
     }
     private boolean dfs(TreeNode root, TreeNode subRoot) {
-        if(root == null && subRoot == null) return true;
-        if((root != null && subRoot == null) || (root == null && subRoot != null) && !isOnlyOne) return false;
-        if(root.val == subRoot.val) {
-            System.out.println(root.val);
-            System.out.println(subRoot.val);
-            return dfs(root.left, subRoot.left) && dfs(root.right, subRoot.right);
-        } else { // 같지 않으니까 subRoot의 루트 값과 같은 노드 발견할 때 까지 깊이 우선 탐색을 한다.
-            return dfs(root.left, subRoot) || dfs(root.right, subRoot);
-        }
+    	if(root == null && subRoot == null) return true;
+    	if(root == null || subRoot == null) return false;
+    	if(root.val != subRoot.val) return false;
+        return dfs(root.left, subRoot.left) && dfs(root.right, subRoot.right);
     }
 
 }
