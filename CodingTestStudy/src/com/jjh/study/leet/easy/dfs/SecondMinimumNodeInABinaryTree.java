a5a0b886932e1db25da8ac02bd455076f39d9356
jinha <jinha4ever@gmail.com>
2022-07-08 15:18:25 +0900

    리트코드 easy: 671. Second Minimum Node In a Binary Tree

--- CodingTestStudy/src/com/jjh/study/leet/easy/dfs/SecondMinimumNodeInABinaryTree.java
+++ CodingTestStudy/src/com/jjh/study/leet/easy/dfs/SecondMinimumNodeInABinaryTree.java
@@ -3,40 +3,21 @@ package com.jjh.study.leet.easy.dfs;
 import com.jjh.study.ds.TreeNode;
 
 public class SecondMinimumNodeInABinaryTree {
-    int secondMinValue = -1;
     public int findSecondMinimumValue(TreeNode root) {
-        if(root.left != null) {
-
-            if(root.left.val == root.right.val) {
-                dfs(root.left, root.right);
-            }
-
-            if(root.left.val > root.right.val) {
-                secondMinValue = root.left.val;
-
-            } else if(root.left.val < root.right.val) {
-                secondMinValue = root.right.val;
-            }
-        }
-        return secondMinValue;
+    	if(root.left == null && root.right == null) return -1;
+    	int left = root.left.val;
+    	int right = root.right.val;
+    	if(left == root.val) {
+    		left = findSecondMinimumValue(root.left);
+    	}
+    	if(right == root.val) {
+    		right = findSecondMinimumValue(root.right);
+    	}
+    	if(left != -1 && right != -1) {
+    		return Math.min(left, right);
+    	}
+    	if(left != -1) return left;
+    	else return right;
     }
-
-    private void dfs(TreeNode leftNode, TreeNode rightNode) {
-        if(leftNode.left == null && rightNode.left == null) {
-            System.out.println("여기?");
-            return;
-        }
-        else if(leftNode.left == null) findSecondMinimumValue(rightNode);
-        else if(rightNode.left == null) findSecondMinimumValue(leftNode);
-        else if(leftNode.left != null && rightNode.left != null) {
-            if(leftNode.left.val == rightNode.left.val) {
-                // 여기 생각해보기
-            }
-            int max1 = Math.max(leftNode.left.val, leftNode.right.val);
-            int max2 = Math.max(rightNode.left.val, rightNode.right.val);
-            secondMinValue = max1 > max2 ? max2 : max1;
-        }
-    }
-
 
 }
